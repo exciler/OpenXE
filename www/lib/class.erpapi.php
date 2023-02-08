7991ae276683fe5cac293a4b490d66ed4b986c18
Andreas Palm <ap@ewsp.de>
2023-02-08 15:00:35 +0100

    check deliverythresholdvatid at the end of Neuberechnen

--- www/lib/class.erpapi.php
+++ www/lib/class.erpapi.php
@@ -36362,6 +36362,13 @@ function Firmendaten($field,$projekt="")
           }
 
         }
+
+        if ($art == 'auftrag') {
+          $dest_country = $this->app->DB->Select("SELECT coalesce(ifnull(lieferland,''), land) FROM auftrag WHERE id='$id' LIMIT 1");
+          $deliverythresholdvatid = $this->app->DB->Select("SELECT ustid FROM lieferschwelle WHERE empfaengerland = '$dest_country' AND verwenden = 1 AND ueberschreitungsdatum <> '' LIMIT 1");
+          if (!empty($deliverythresholdvatid))
+            $this->app->DB->Update("UPDATE auftrag SET deliverythresholdvatid='$deliverythresholdvatid' WHERE id='$id' LIMIT 1");
+        }
         $this->RunHook('ANABREGSNeuberechnenEnde',2, $id, $art);
       }
 
