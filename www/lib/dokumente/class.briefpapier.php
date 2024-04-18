c6f5bc66bd21681cf1ecd72ef0cd654cd29bca57
Andreas Palm <ap@ewsp.de>
2024-04-18 12:48:03 +0200

    Remove Ioncube and License stuff

--- www/lib/dokumente/class.briefpapier.php
+++ www/lib/dokumente/class.briefpapier.php
@@ -97,10 +97,7 @@ class Briefpapier extends SuperFPDF {
 
     $hintergrund = $this->getStyleElement('hintergrund');
 
-    if(!empty(erpAPI::Ioncube_Property('isdevelopmentversion'))) {
-      $this->setDevelopmentVersionBackground();
-    }
-    elseif($this->app->erp->BriefpapierHintergrunddisable)
+    if($this->app->erp->BriefpapierHintergrunddisable)
     {
     }
     else if($hintergrund=='logo')
