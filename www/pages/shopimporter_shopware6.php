1c6732344e6a93a7f9d9e5daf43c5f80ffdbccb5
Andreas Palm <ap@ewsp.de>
2024-07-09 11:51:50 +0200

    Shopware 6: Export translations for variants

--- www/pages/shopimporter_shopware6.php
+++ www/pages/shopimporter_shopware6.php
@@ -2901,6 +2901,9 @@ class Shopimporter_Shopware6 extends ShopimporterBase
             }
 
             $this->addCoverImage($variantImageData, $variantProductId);
+            if($variant['texteuebertragen']) {
+                $this->exportTranslationsForArticle($variant, $variantProductId);
+            }
         }
 
         $existingConfigurations = $this->shopwareRequest(
