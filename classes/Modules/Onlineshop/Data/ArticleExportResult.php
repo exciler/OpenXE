b37222f16bff26f651f459ab89048a13b6862b79
Andreas Palm <ap@ewsp.de>
2025-02-05 00:17:36 +0100

    Improvements and Standardization of Article-Shopexport

--- classes/Modules/Onlineshop/Data/ArticleExportResult.php
+++ classes/Modules/Onlineshop/Data/ArticleExportResult.php
@@ -3,8 +3,8 @@
 namespace Xentral\Modules\Onlineshop\Data;
 
 class ArticleExportResult {
-    public int $articleId;
+    public int $articleId = 0;
     public bool $success = false;
-    public ?string $message;
-    public ?string $extArticleId;
+    public ?string $message = null;
+    public ?string $extArticleId = null;
 }
