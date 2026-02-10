e1cb4028ed8faa81c2f90f515d600349227215ed
Andreas Palm <ap@ewsp.de>
2026-02-10 21:31:42 +0100

    Combine autoloader

--- xentral_autoloader.php
+++ xentral_autoloader.php
@@ -2,36 +2,11 @@
 
 function xentral_autoloader($class) {
   $classes = array(
-    'erpooSystem'=>__DIR__.'/www/eproosystem.php',
-    'erpAPICustom'=>__DIR__.'/www/lib/class.erpapi_custom.php',
-    'RemoteCustom'=>__DIR__.'/www/lib/class.remote_custom.php',
-    'HttpClient'=>__DIR__.'/www/lib/class.httpclient.php',
-    'DatabaseUpgrade'=>__DIR__.'/phpwf/plugins/class.databaseupgrade.php',
-    'PDF_EPS'=>__DIR__.'/www/lib/pdf/fpdf_final.php',
-    'SuperFPDF'=>__DIR__.'/www/lib/dokumente/class.superfpdf.php',
-    'Briefpapier'=>__DIR__.'/www/lib/dokumente/class.briefpapier.php',
     'PDF'=>__DIR__.'/www/lib/pdf/fpdf.php',
-    'SpeditionPDF'=>__DIR__.'/www/lib/dokumente/class.spedition.php',
-    'EtikettenPDF'=>__DIR__.'/www/lib/dokumente/class.etiketten.php',
-    'Dokumentenvorlage'=>__DIR__.'/www/lib/dokumente/class.dokumentenvorlage.php',
-    'SepaMandat'=>__DIR__.'/www/lib/dokumente/class.sepamandat.php',
-    'WikiParser'=>__DIR__.'/www/plugins/class.wikiparser.php',
-    'IndexPoint'=>__DIR__.'/www/plugins/class.wikiparser.php',
-    'phpprint'=>__DIR__.'/www/plugins/php-print.php',
-    'LiveimportBase'=>__DIR__.'/www/plugins/liveimport/LiveimportBase.php',
   );
   if(isset($classes[$class]) && is_file($classes[$class]))
   {
     include_once $classes[$class];
-    return;
-  }
-  if($class === 'AES')
-  {
-    if(version_compare(phpversion(),'7.1', '>=') && is_file(__DIR__.'/www/lib/class.aes2.php')){
-        include __DIR__.'/www/lib/class.aes2.php';
-    } else{
-      include __DIR__ . '/www/lib/class.aes.php';
-    }
     return;
   }
   if($class === 'FPDFWAWISION')
@@ -64,34 +39,6 @@ function xentral_autoloader($class) {
       require_once __DIR__ .'/www/lib/pdf/fpdf.php';
     }
     return;
-  }
-  if($class === 'BriefpapierCustom')
-  {
-    if(is_file(__DIR__.'/www/lib/dokumente/class.briefpapier_custom.php'))
-    {
-      include __DIR__.'/www/lib/dokumente/class.briefpapier_custom.php';
-    }else{
-      class BriefpapierCustom extends Briefpapier
-      {
-
-      }
-    }
-  }
-
-  if(substr($class, -3) === 'PDF') {
-    $file = __DIR__.'/www/lib/dokumente/class.'.strtolower(substr($class,0,-3)).'.php';
-    if(file_exists($file)) {
-      include $file;
-    }
-    elseif(file_exists(__DIR__.'/www/lib/dokumente/class.'.strtolower($class).'.php')) {
-      include __DIR__.'/www/lib/dokumente/class.'.strtolower($class).'.php';
-    }
-  }
-  elseif(substr($class, -9) === 'PDFCustom') {
-    $file = __DIR__.'/www/lib/dokumente/class.'.strtolower(substr($class,0,-9)).'_custom.php';
-    if(file_exists($file)) {
-      include $file;
-    }
   }
 }
 
